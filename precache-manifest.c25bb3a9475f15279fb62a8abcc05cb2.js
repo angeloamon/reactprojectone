fb947dad8756083dfed0046fdf3857f048c7b997
coffeemaker30 <angeloamon@gmail.com>
2019-05-10 20:20:20 +0800

    Updates

---
+++ precache-manifest.c25bb3a9475f15279fb62a8abcc05cb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b935a476c36359db826acd13eb38260f",
+    "url": "/reactprojectone/index.html"
+  },
+  {
+    "revision": "266420167d975a8b8369",
+    "url": "/reactprojectone/static/css/main.84a9c081.chunk.css"
+  },
+  {
+    "revision": "1afa6feabec7c37a57e4",
+    "url": "/reactprojectone/static/js/2.7e421706.chunk.js"
+  },
+  {
+    "revision": "266420167d975a8b8369",
+    "url": "/reactprojectone/static/js/main.0cf24bfb.chunk.js"
+  },
+  {
+    "revision": "b243cfc6ac6276148b66",
+    "url": "/reactprojectone/static/js/runtime~main.6ba39f6f.js"
+  }
+]);
